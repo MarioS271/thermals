f5cd4b7f972a6f633d7dda4e54bb58ce24c5c3e2
MarioS271 <marioerik.scholze@gmail.com>
2026-06-21 22:26:10 +0200

    feat(tray-icon): color maps to temperature now, added antialiasing and made the rect rounded

--- src/main/java/net/marios271/thermals/tray/TrayIconDrawer.java
+++ src/main/java/net/marios271/thermals/tray/TrayIconDrawer.java
@@ -4,14 +4,30 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class TrayIconDrawer {
+    final int cornerRadius = 10;
+
+    final float coldHue = 0.33f;
+    final float hotHue = 0f;
+    final int coldTemp = 30;  // °C
+    final int hotTemp = 90;  // °C
+
     public BufferedImage draw(int tempCelsius, Dimension iconSize) {
         BufferedImage img = new BufferedImage(iconSize.width, iconSize.height, BufferedImage.TYPE_INT_ARGB);
         Graphics2D graphics = img.createGraphics();
 
-        graphics.setColor(Color.WHITE);
-        graphics.fillRect(0, 0, iconSize.width, iconSize.height);
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        graphics.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+
+        graphics.setColor(generateColor(tempCelsius));
+        graphics.fillRoundRect(0, 0, iconSize.width, iconSize.height, cornerRadius, cornerRadius);
         graphics.dispose();
 
         return img;
+    }
+
+    private Color generateColor(int temp) {
+        float t = (float) (temp - coldTemp) / (hotTemp - coldTemp);
+        float hue = coldHue + t * (hotHue - coldHue);
+        return Color.getHSBColor(hue, 1f, 1f);
     }
 }
