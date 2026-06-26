6d3a144331b065e6c8302ab95b039929d7335c2a
MarioS271 <marioerik.scholze@gmail.com>
2026-06-26 09:38:29 +0200

    feat(ui): adjusted color scheme again

--- src/main/java/net/marios271/thermals/ui/UICommons.java
+++ src/main/java/net/marios271/thermals/ui/UICommons.java
@@ -3,9 +3,9 @@ package net.marios271.thermals.ui;
 import java.awt.*;
 
 public final class UICommons {
-    public static final Color WINDOW_BACKGROUND_COLOR = new Color(0x2C2C2E);
+    public static final Color WINDOW_BACKGROUND_COLOR = new Color(0x222224);
 
-    public static final Color PANEL_BACKGROUND_COLOR = new Color(0x232323);
+    public static final Color PANEL_BACKGROUND_COLOR = new Color(0x404042);
     public static final Dimension BIG_PANEL_SIZE = new Dimension(260, 160);
     public static final Dimension SMALL_PANEL_SIZE = new Dimension(200, 80);
     public static final int PANEL_SPACING = 10;
