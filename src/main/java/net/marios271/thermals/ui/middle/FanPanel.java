435d962ed7e4a1ad9009de29a949994124e3de54
MarioS271 <marioerik.scholze@gmail.com>
2026-07-04 18:15:20 +0200

    feat(ui): implement fan panel

--- src/main/java/net/marios271/thermals/ui/middle/FanPanel.java
+++ src/main/java/net/marios271/thermals/ui/middle/FanPanel.java
@@ -2,12 +2,37 @@ package net.marios271.thermals.ui.middle;
 
 import net.marios271.thermals.ui.UICommons;
 import net.marios271.thermals.ui.components.ComponentPanel;
+import net.marios271.thermals.ui.components.Stat;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
 
 public class FanPanel extends ComponentPanel {
+    ArrayList<Stat> fans = new ArrayList<>();
 
     public FanPanel() {
         super("Fan Speeds");
 
-        setAllSizes(UICommons.DEFAULT_PANEL_SIZE);
+        fans.add(new Stat("3500", "RPM", "Fan 1"));
+        fans.add(new Stat("3500", "RPM", "Fan 2"));
+        fans.add(new Stat("3500", "RPM", "Fan 3"));
+
+        int cols = (fans.size() + 1) / 2;
+        int width = cols * (UICommons.PANEL_WIDTH / 3);
+        setAllSizes(new Dimension(
+            Math.max(width, UICommons.PANEL_WIDTH),
+            UICommons.COMPONENT_PANEL_HEIGHT)
+        );
+
+        JPanel stats = new JPanel();
+        stats.setLayout(new GridLayout(2, cols));
+        stats.setBackground(UICommons.PANEL_BACKGROUND_COLOR);
+
+        for (Stat fan : fans) {
+            stats.add(fan);
+        }
+
+        add(stats);
     }
 }
